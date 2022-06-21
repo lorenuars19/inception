846b0cbbfdfc0bfa1bca63b7799167041de54b37
lorenuars19 <lorenuars19@gmail.com>
2022-06-21 17:11:30 +0000

    fix: db user me being dumb

--- srcs/database/entry.sh
+++ srcs/database/entry.sh
@@ -31,11 +31,11 @@ FLUSH PRIVILIEGES;
 
 EOF
 
-/usr/bin/mysqld --user=${LOGIN} --bootstrap < ${tmp}
+/usr/bin/mysqld --user=mysql --bootstrap < ${tmp}
 
 # allow remote connections
 sed -i "s|skip-networking|# skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
 
 
-exec /usr/bin/mysqld --user=${LOGIN} --console
+exec /usr/bin/mysqld --user=mysql --console
