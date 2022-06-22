61d15b188d366c1a166cccae20895e7fdd20d911
lorenuars19 <lorenuar.s19@gmail.com>
2022-06-22 17:13:18 +0200

    fix: wordpress again

--- srcs/wordpress/entry.sh
+++ srcs/wordpress/entry.sh
@@ -4,8 +4,8 @@ curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -
 && chmod 700 /bin/wp \
 && wp cli update \
 && wp --allow-root core download --path=${VOL_WP_PATH} --locale=en_US \
-&& wp --allow-root config create --path=${VOL_WP_PATH} --dbname=${LOGIN}_wp --dbuser=${LOGIN} --dbpass=${MY_SQL_ROOT_PASSWD}
-&& wp --allow-root core install --path=${VOL_WP_PATH} --url=${LOGIN}.42.fr --title="${LOGIN}_site" --admin_user=${LOGIN} --admin_password=${WP_ROOT_PASSWD} --admin_email="${LOGIN}@student.s19.be"
-&& wp --allow-root user create --path=${VOL_WP_PATH} --user-login=edit --role=editor --user_pass=${WP_EDIT_PASSWD}
+&& wp --allow-root config create --path=${VOL_WP_PATH} --dbname=${LOGIN}_wp --dbuser=${LOGIN} --dbpass=${MY_SQL_ROOT_PASSWD} \
+&& wp --allow-root core install --path=${VOL_WP_PATH} --url=${LOGIN}.42.fr --title="${LOGIN}_site" --admin_user=${LOGIN} --admin_password=${WP_ROOT_PASSWD} --admin_email="${LOGIN}@student.s19.be" \
+&& wp --allow-root user create --path=${VOL_WP_PATH} --user-login=edit --role=editor --user_pass=${WP_EDIT_PASSWD} \
 
 wp shell
